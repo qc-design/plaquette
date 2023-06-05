9d5f9476c1f901326fddc9f42a760cdcc67e915b
Marcello Massaro <18736562+massarom@users.noreply.github.com>
2023-06-05 09:33:22 +0200

    chore: update copyright (#13)

--- src/plaquette/__main__.py
+++ src/plaquette/__main__.py
@@ -1,4 +1,4 @@
-# Copyright 2023, It'sQ GmbH and the plaquette contributors
+# Copyright 2023, QC Design GmbH and the plaquette contributors
 # SPDX-License-Identifier: Apache-2.0
 """Entry-point module to recover plaquette's version quickly."""
 import plaquette
--- src/plaquette/device/_stimsim.py
+++ src/plaquette/device/_stimsim.py
@@ -1,4 +1,4 @@
-# Copyright 2023, It'sQ GmbH and the plaquette contributors
+# Copyright 2023, QC Design GmbH and the plaquette contributors
 # SPDX-License-Identifier: Apache-2.0
 """Interface to Stim for use as circuit simulator.
 
